c10442cfb563cb74f57a165c7531e17f5cfa9312
Josue Valverde <josuevalverde12@gmail.com>
2015-08-14 09:47:06 -0700

    add parentheses to length() method to stay consistent.

--- lib/word.rb
+++ lib/word.rb
@@ -5,7 +5,7 @@ class Word
 
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word).capitalize()
-    @id = @@words.length + 1
+    @id = @@words.length() + 1
   end
 
   define_method(:save) do
