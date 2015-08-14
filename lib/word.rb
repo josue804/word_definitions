d5ad66b326a1e6ee756468831cadbc79629dc835
Josue Valverde <josuevalverde12@gmail.com>
2015-08-14 13:32:06 -0700

    add code to save a definition object onto a word.

--- lib/word.rb
+++ lib/word.rb
@@ -1,15 +1,20 @@
 class Word
 
-  attr_reader(:word, :id)
+  attr_reader(:word, :id, :definitions)
   @@words = []
 
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word).capitalize()
     @id = @@words.length() + 1
+    @definitions = []
   end
 
   define_method(:save) do
     @@words.push(self)
+  end
+
+  define_method(:save_definition) do |definition|
+    @definitions.push(definition)
   end
 
   define_singleton_method(:clear) do
